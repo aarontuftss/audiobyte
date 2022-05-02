d8104100c208563dcdd9c86899c0fa983d71e30c
Jonathon Tufts <86862338+jonathontufts@users.noreply.github.com>
2022-05-02 16:55:33 -0600

    log in & sign up pages

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -28,30 +28,37 @@ function LoginFormPage() {
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <ul>
-                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-            </ul>
-            <label>
-                Username or Email
-                <input
-                    type="text"
-                    value={credential}
-                    onChange={(e) => setCredential(e.target.value)}
-                    required
-                />
-            </label>
-            <label>
-                Password
-                <input
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                />
-            </label>
-            <button type="submit">Log In</button>
-        </form>
+        <div className='mainWrap'>
+            <div className='imgDisplay'>
+                <h1>Welcome Back</h1>
+                <div className='formDiv'>
+                    <form onSubmit={handleSubmit} className='form1'>
+                        <ul>
+                            {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                        </ul>
+                        <label>
+                            Username or Email
+                            <input
+                                type="text"
+                                value={credential}
+                                onChange={(e) => setCredential(e.target.value)}
+                                required
+                            />
+                        </label>
+                        <label>
+                            Password
+                            <input
+                                type="password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </label>
+                        <button type="submit">Log In</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
 
--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -6,47 +6,48 @@ import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
     const dispatch = useDispatch();
-    const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(true);
 
-    const openMenu = () => {
-        if (showMenu) return;
-        setShowMenu(true);
-    };
+    // const openMenu = () => {
+    //     if (showMenu) return;
+    //     setShowMenu(true);
+    // };
 
-    useEffect(() => {
-        if (!showMenu) return;
+    // useEffect(() => {
+    //     if (!showMenu) return;
 
-        const closeMenu = () => {
-            setShowMenu(false);
-        };
+    //     const closeMenu = () => {
+    //         setShowMenu(false);
+    //     };
 
-        document.addEventListener('click', closeMenu);
+    //     document.addEventListener('click', closeMenu);
 
-        return () => document.removeEventListener("click", closeMenu);
-    }, [showMenu]);
+    //     return () => document.removeEventListener("click", closeMenu);
+    // }, [showMenu]);
 
     const logout = (e) => {
         e.preventDefault();
         dispatch(sessionActions.logout());
         window.location.assign("/")
-
-
     };
 
     return (
         <>
-            <button onClick={openMenu}>
-                <i className="fas fa-user-circle" />
-            </button>
-            {showMenu && (
-                <ul className="profile-dropdown">
-                    <li>{user.username}</li>
-                    {/* <li>{user.email}</li> */}
-                    <li>
-                        <button onClick={logout}>Log Out</button>
-                    </li>
-                </ul>
-            )}
+            <div className='userWrap'>
+                {/* <button onClick={openMenu}> */}
+                <button>
+                    <i className="fas fa-user-circle" />
+                </button>
+                {showMenu && (
+                    <ul className="profile-dropdown">
+                        <li>{user.username}</li>
+                        {/* <li>{user.email}</li> */}
+                        <li>
+                            <button onClick={logout} className='logoutB'>Log Out</button>
+                        </li>
+                    </ul>
+                )}
+            </div>
         </>
     );
 }
